3fe5292b423381cd7c92f8726d916dc8cb8e2885
Administrator <Administrator@Superintendent>
2011-06-01 19:55:55 -0700

    Fixed AntiZombieRender and basic Anti death animations

--- src/edu/csupomona/kyra/component/render/ai/AntiZombieRender.java
+++ src/edu/csupomona/kyra/component/render/ai/AntiZombieRender.java
@@ -7,24 +7,34 @@ import org.newdawn.slick.SlickException;
 public class AntiZombieRender extends AIRender {
 	public AntiZombieRender(String id) throws SlickException {
 		super(id);
-		Image[] bERightmovement = {
+		int[] moveTween = {200, 200, 200, 200};
+		Image[] moveRight = {
 				new Image("img/anti-enemy-move-right_001.png"),
 				new Image("img/anti-enemy-move-right_002.png"),
 				new Image("img/anti-enemy-move-right_001.png"),
 				new Image("img/anti-enemy-move-right_003.png")
 		};
-		Image[] bELeftmovement = {
+		Image[] moveLeft = {
 				new Image("img/anti-enemy-move-left_001.png"),
 				new Image("img/anti-enemy-move-left_002.png"),
 				new Image("img/anti-enemy-move-left_001.png"),
 				new Image("img/anti-enemy-move-left_003.png")
 		};
-		int[] movementDuration = {200, 200, 200, 200};
+		setMoveRight(new Animation(moveRight, moveTween, false));
+		setMoveLeft(new Animation(moveLeft, moveTween, false));
 		
-		animations = new Animation[2];
-		animations[0] = new Animation(bELeftmovement, movementDuration, false);
-		animations[1] = new Animation(bERightmovement, movementDuration, false);
+		int[] deathTween = {500,175};
+		Image[] deathRight = {
+				new Image("img/anti-enemy-death-right-1.png"),
+				new Image("img/anti-enemy-death-right-2.png")
+		};
+		Image[] deathLeft = {
+				new Image("img/anti-enemy-death-left-1.png"),
+				new Image("img/amti-enemy-death-left-2.png")
+		};
+		setDeathRight(new Animation(deathRight, deathTween, false));
+		setDeathLeft(new Animation(deathLeft, deathTween, false));
 		
-		super.setAnimations(animations);
+		setSprite(super.moveLeft);
 	}
 }
