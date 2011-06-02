d3ac583825719d492a6e59b6eaee7d90cf6b8d0b
Anthony Mendez <almendez@csupomona.edu>
2011-06-01 18:00:54 -0700

    Player's bullets are now animated. Added necessary frames.

--- src/edu/csupomona/kyra/component/render/bullet/PlayerBulletRender.java
+++ src/edu/csupomona/kyra/component/render/bullet/PlayerBulletRender.java
@@ -1,17 +1,33 @@
 package edu.csupomona.kyra.component.render.bullet;
 
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 import edu.csupomona.kyra.component.render.ImageRender;
+import edu.csupomona.kyra.component.render.SpriteRender;
 
-public class PlayerBulletRender extends ImageRender {
+public class PlayerBulletRender extends SpriteRender {
 
 	public PlayerBulletRender(String id) throws SlickException {
 		super(id);
 		
-		Image bullet = new Image("img/fireball.png");
+		Image[] bulletAnimation = {
+				new Image("img/bullet-1.png"),
+				new Image("img/bullet-2.png")
+		};
 		
-		super.setImage(bullet);
+		int[] frameDuration = {50, 50};
+		
+		animations = new Animation[1];
+		animations[0] = new Animation(bulletAnimation, frameDuration, true);
+		super.setAnimations(animations);
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sb, int delta) {
+		
 	}
 }
